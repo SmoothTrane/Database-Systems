d46884690d1d9726037c1df52653d14b188c53c9
Gary <gary.coltrane1@marist.edu>
2017-02-07 21:40:47 -0500

    added a few queries

--- lab4/lab4.sql
+++ lab4/lab4.sql
@@ -16,7 +16,10 @@ WHERE agents.aid IN (select orders.aid
 
 -- 2
 
-SELECT DISTINCT
+
+
+SELECT DISTINCT pid
+FROM products 
 
 
 -- 3
@@ -26,10 +29,39 @@ SELECT DISTINCT
 
 SELECT distinct cid, name 
 FROM customers
-WHERE NOT customers.cid  IN(SELECT orders.cid FROM ORDERS WHERE aid = 'a01') ORDER BY CUSTOMERS.cid;
+WHERE NOT customers.cid  IN (SELECT orders.cid 
+														FROM ORDERS 
+														WHERE aid = 'a01') 
+ORDER BY CUSTOMERS.cid;
 
 
 
 --4
 
-SELECT cid from customers where
+
+
+SELECT customers.cid 
+from customers 
+where customers.cid in 
+										(SELECT distinct orders.cid 
+											from ORDERS
+											 WHERE pid = 'p01' 
+											 INTERSECT 
+											 SELECT orders.cid 
+											 from ORDERS 
+											 where pid = 'p07');
+
+
+
+--5
+
+
+
+
+-- 6
+
+SELECT aid from agents where city in("Tokyo"), "New York");
+
+;
+
+SELECT name, discount, city from customers where customers.cid IN(SELECT orders.cid from orders where orders.aid IN (SELECT aid from agents where city in('Tokyo', 'New York');
