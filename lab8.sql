6226bbbdbbbf5cc3a9eaca2e38a6cf9e2e512d47
Gary <gary.coltrane1@marist.edu>
2017-03-27 23:31:57 -0400

    lab submission

--- lab8.sql
+++ lab8.sql
@@ -1,38 +1,72 @@
-  CREATE TABLE actors(
-    aid int not null,
-    name text,
-    address text,
-    dob date,
-    ActorsGuildAnniverseryDate date
-);
 
-CREATE TABLE actors_personal_features(
-  HairColor text,
-  EyeColor text,
-  FavoriteColor text,
-  heightInches int,
-  weight int,
+CREATE TABLE People(
+  PID char(8) NOT NULL,
+  Name text not null,
+  Address text,
   SpouseName text,
+  primary key (pid)
+);
+/*
+Functional Dependencies, PID -> name, address, spousename
+*/
 
+  CREATE TABLE Actors(
+    PID  char(8) references People(pid) NOT NULL,
+    DOB date,
+    HairColor text,
+    EyeColor text,
+    FavoriteColor text,
+    HeightInches int,
+    WeightLbs int,
+    ActorsGuildAnniverseryDate date,
+    primary key(pid)
+);
+/*
+Functional Dependencies
+pid -> dob, haircolor, eyecolor, favoritecolor, heightinches, weoght pounds, actors guild anniversary date
+*/
+
+CREATE TABLE Movies(
+  MPAA_Number integer not null,
+  Name text,
+  ReleaseYear int,
+  DomesticBoxOfficeSalesUSD integer,
+  ForeignBoxOfficeSalesUSD integer,
+  DVD_BLURAY_SALESUSD integer,
+  primary key (MPAA_NUMBER)
 );
 
-CREATE TABLE movies(
-  mid int not null auto_increment,
-  name text,
-  ReleaseYear date.
-  MPAA_Number integer,
-  DomesticBoxOfficeSales integer,
-  ForeignBoxOfficeSales integer,
-  sales integer
-);
 
-CREATE TABLE directors(
-  did int not null auto_increment,
-  name text,
-  address text,
-  SpouseName text,
+/*
+MPAA -> name, releaseyear, domesticboxoffice, foreignboxoffice, dvd blue ray
+*/
+
+CREATE TABLE Directors(
+ PID char(8) references people(pid) NOT null,
   FilmSchool text,
   DirectorsGuildAnniversaryDate date,
-  FavoriteLensMaker
-
+  FavoriteLensMaker text
+  primary key (pid)
 );
+
+
+/*
+PID -> name, address, spousename, filmschool, directorsguildanniversary date, favoritelensmaker
+*/
+
+CREATE table Casting(
+  MPAA_number integer references movies(MPAA_number) not null,
+  pid char(8) references People(pid) not null,
+  RoleName text not null,
+  primary key (pid, MPAA_number)
+);
+
+/*
+MPAA_NUMBER, PID -> rolename
+*/
+SELECT people.name
+FROM people INNER JOIN casting ON casting.pid = people.pid AND
+INNER JOIN DIRECTORS on people.pid = directors.pid
+AND casting.mpaa_number IN ( SELECT casting.mpaa_number
+                        FROM casting INNER JOIN people ON people.pid = casting.pid
+                        WHERE people.name = 'Sean Connery' ) ;
