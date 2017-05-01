ebbb8a1c95e8e387f2049551d069e2fe7b0a12df
Gary <gary.coltrane1@marist.edu>
2017-05-01 08:04:18 -0400

    added triggers and new tables

--- BullyBoyTerrierr/data.sql
+++ BullyBoyTerrierr/data.sql
@@ -91,6 +91,21 @@ CREATE TABLE DogPurchases(
     primary key (DogPurchaseID),
     foreign key (PurchaseID) references Purchases (PurchaseID)
 );
+Create table Shots(
+  ShotID  char(8) not null unique,
+  name text,
+  primary key (ShotID)
+);
+
+Create table dogshots(
+  DogShotID char(8) not null unique,
+  ShotID char(8) not null,
+  DogID char(8) not null,
+  ShotDate date,
+  primary key (DogShotID),
+  foreign key (ShotID) references Shots (ShotID)
+  foreign key (DogID) references Dogs (DogID);
+);
 
 INSERT INTO PEOPLE (PersonID, name) VALUES
 ('P0000001', 'Byron'), ('P0000002', 'Gary'), ('P0000003', 'Bryant'), ('P0000004', 'Bryce'),('P0000005', 'Abdul'), ('P0000006', 'Sharif'), ('P0000007', 'Amar'), ('P0000008', 'Muhammad'),('P0000009', 'Peewee'), ('P0000010', 'Tyshawn'), ('P0000011', 'Jay P'),
@@ -154,21 +169,10 @@ INSERT INTO StudPurchases(StudPurchaseID, PurchaseID) VALUES
 INSERT INTO DogPurchases(DogPurchaseID, PurchaseID ) VALUES
 ('DP000001','PU000005'), ('DP000002','PU000014'), ('DP000003','PU000005'), ('DP000004','PU000008'), ('DP000005','PU000009'),
 ('DP000006','PU000010');
--- CREATE OR REPLACE FUNCTION get_breeders_dogs (char(4), REFCURSOR) returns refcursor as
--- $$
--- DECLARE
---   breeder_ID   char(4)     := $1
---   results     REFCURSOR   := $2
 
--- BEGIN
---     OPEN results for
---     SELECT breederID, breederName from breeder  inner join on dogs  where  breeder_id = dogs.breederid
--- return results;
---end;
--- $$
---language plpgsql;
---
 
+INSERT INTO SHOTS(ShotID, Name) VALUES
+('S00000O1', 'Rabies'),('S00000O2', 'Lyme'),('S00000O3', 'Bordetella'),('S00000O4', 'Giardia');
 --CREATE OR REPLACE FUNCTION get dog age
 
 -- create a function that compares two trainers, and returns which trainer has trained the most expensive dogs
@@ -178,35 +182,57 @@ INSERT INTO DogPurchases(DogPurchaseID, PurchaseID ) VALUES
 
 
 
-CREATE OR REPLACE FUNCTION most_expensive_dogs (char(4), char(4), REFCURSOR) returns refcursor as
+CREATE OR REPLACE FUNCTION get_breeders_dogs (char(8), REFCURSOR) returns refcursor as
 $$
-DECLARE
-  TrainerID1   char(4)        := $1
-  TrainerID2   char(4)        := $2
-  results      REFCURSOR      := $3
-  BEGIN
-  SELECT name from breeders where trainer.breederid = trainerID1 inner join dogs on trainerid1 = dogs.trainerid inner join purchases on dogs.dogid = purchaes.dogid   union
-  (select name from breeders where breeder.breederid = trainerid2 inner join dogs on trainerid2 = dogs.trainerid inner join purchases on dogs.dogid = purchaes.dogid) order by purchases.priceusd;
+ DECLARE
+   breeder_ID   char(8)     := $1;
+   results     REFCURSOR   := $2;
 
-  END
-$$
-language plpgsql
+ BEGIN
+     OPEN results for
+     SELECT dogs.dogid, dogs.name from people  inner join breeders on people.personid = breeders.personid
+     inner join dogs on dogs.breederid= breeders.breederid
+     where breeders.breederid = breeder_ID;
+ return results;
+end;
+ $$
+language plpgsql;
 
-CREATE OR REPLACE FUNCTION update_puppy_status()
-RETURNS TRIGGER AS
+CREATE OR REPLACE FUNCTION get_dogs_shots (char(8), REFCURSOR) returns refcursor as
 $$
+ DECLARE
+   DogID   char(8)     := $1;
+   results     REFCURSOR   := $2;
+
+ BEGIN
+     OPEN results for
+     select shots.name as Available shots, dogshots.shotdate from shots inner join dogshots on shots.shotsid = dogshots.id
+     where dogshots.dogid = DogID;
+ return results;
+end;
+ $$
+language plpgsql;
+
+
+
+
+
 
 CREATE OR REPLACE FUNCTION puppyAge(char(8))
 returns interval as
 $$
 declare
-puppyID := char(8)
-birthday date := (select dogs.dob from dogs inner join puppy on dogs.dogid = puppy.dogid where dogs.dogid = puppy.dogid);
+pupID char(8) := $1;
+birthday date := (select dogs.dob from dogs inner join puppies on dogs.dogid = puppies.dogid where puppies.PUPPYid = PupID);
 BEGIN
-return age(birthday)
- end
+return age(birthday);
+ end;
  $$
  language plpgsql;
+
+ select puppyAge('PP000001');
+
+
 
 
 CREATE OR REPLACE FUNCTION most_children(adultdogid char(8))
@@ -223,25 +249,45 @@ language plpgsql;
 
 --VIEW: List all of the puppies for sale
 
-/*
+
 CREATE VIEW AvailablePuppiesForSale  AS
 SELECT name, weightlbs, gender from dogs inner join puppies on dogs.dogid = puppies.dogid
 where dogs.forsale = 'TRUE';
-*/
+
 
 
 -- POSSIBLE TRIGGERS: WHENEVER A NEW  PUPPY IS PURCHASED, automatically set, forSALE to be SOLD
 
 
+
+
+CREATE OR REPLACE FUNCTION setPuppyAsSold()
+returns trigger as $$
+BEGIN
+  if new.dogid is not null then
+    update dogs
+      set ForSale = 'SOLD'
+      where dogs.dogid = New.dogid;
+  end if
+  return new;
+end;
+$$ language plpgql;
+
+
+Create Trigger soldPuppy
+before UPDATE on Purchases
+for each row
+execute procedure setPuppyAsSold();
+
+
+
+CREATE OR REPLACE function
+
 -- VIEWS SELECT THE  dog with the most children, this is for people that are interested in the lineage of a specfic dog
 
 --
 
--- CREATE VIEW CurrentDogs  AS
--- SELECT name, dob, gender, weightlbs,  from dogs inner join adultdogs on dogs.dogid = puppies.dogid
--- where dogs.forsale = "true"
--- order by desc;
--- -- view the trainer that has trained the most expensive dog
+
 
 /* View the current trainer that sold the most expensive stud */
 
@@ -272,4 +318,20 @@ grant select on dogs, adultdogs, puppies, breeds;
 
 -- REPORTS
 
---QUERY TO RETURN the percentage of dogs that are pitbulls
+--QUERY TO RETURN the percentage of dogs that are adults and are pitbulls
+SELECT	TRUNC (CAST((SELECT	COUNT(dogs.dogid)	AS	PitbullsNum
+FROM	dogs
+inner join adultdogs on dogs.dogid = adultdogs.dogid
+WHERE	dogs.breedid= 'BR000003')	as decimal(5,2))
+ /
+ (SELECT	COUNT(dogs.dogid)	AS	allDogs FROM	dogs)	*	100	)
+ as	AdultPitbullsPercent
+
+
+
+
+
+-- Puppies that are trained by people who have trained a lot of dogs
+
+
+select * from dogs inner join puppies on dogs.dogid = puppies.dogid
