c2d95e606671fc91a014a88cc6389b8421e47ed0
Gary <gary.coltrane1@marist.edu>
2017-04-30 20:53:24 -0400

    made some sql updates

--- BullyBoyTerrierr/data.sql
+++ BullyBoyTerrierr/data.sql
@@ -40,11 +40,11 @@ CREATE TABLE Dogs(
   BreederID char(8),
   BreedID char(8) not null,
   TrainerID char(8),
-  DOB date not null,
+  DOB date,
   MotherID char(8),
   FatherID char(8),
   WeightLBS int,
-  gender text check (gender in ('Male', 'Female')),
+  gender text check (gender in ('MALE', 'FEMALE')),
   ForSale text check (ForSale in ('SOLD', 'FALSE', 'TRUE')),
   primary key (DogID),
   foreign key (BreederID) references Breeders(BreederID),
@@ -92,20 +92,20 @@ CREATE TABLE DogPurchases(
     foreign key (PurchaseID) references Purchases (PurchaseID)
 );
 
-INSERT INTO PEOPLE (PeopleID, name) VALUES
+INSERT INTO PEOPLE (PersonID, name) VALUES
 ('P0000001', 'Byron'), ('P0000002', 'Gary'), ('P0000003', 'Bryant'), ('P0000004', 'Bryce'),('P0000005', 'Abdul'), ('P0000006', 'Sharif'), ('P0000007', 'Amar'), ('P0000008', 'Muhammad'),('P0000009', 'Peewee'), ('P0000010', 'Tyshawn'), ('P0000011', 'Jay P'),
 ('P0000012', 'David'), ('P0000013', 'Mary'), ('P0000014', 'Yusuf'), ('P0000015', 'Abraham'), ('P0000016', 'Jermaine'), ('P0000017', 'Ana'), ('P0000018', 'Khadijah'), ('P0000019', 'Clara'), ('P0000020', 'Betty'),('P0000021', 'Jacky'), ('P0000022', 'Omar'), ('P0000023', 'Mitch'),
-('P0000024', 'Akbar'), ('P0000025', 'Kareem'), ('P0000026', 'Tyrone'),('P0000027', 'Trey'),;
+('P0000024', 'Akbar'), ('P0000025', 'Kareem'), ('P0000026', 'Tyrone'),('P0000027', 'Trey');
 INSERT INTO TRAINERS (TrainerID, PersonID) VALUES
-('T0000001', 'P0000001'), ('T0000002', 'P0000003'), ('T0000003', 'P0000005'), ('T0000004', 'P0000008'), ('T0000005', 'P0000012'), ('T0000006', 'P0000014'),('T0000006', 'P0000027');
+('T0000001', 'P0000001'), ('T0000002', 'P0000003'), ('T0000003', 'P0000005'), ('T0000004', 'P0000008'), ('T0000005', 'P0000012'), ('T0000006', 'P0000014'),('T0000007', 'P0000027');
 
 Insert into Breeders (BreederID, PersonID) VALUES
 ('B0000001', 'P0000001'), ('B0000002', 'P0000003'), ('B0000003', 'P0000002'), ('B0000004', 'P0000004'), ('B0000005', 'P0000005'), ('B0000006', 'P0000025'),('B0000007', 'P0000026');
 
 INSERT INTO Customers(CustomerID, PersonID ) VALUES
-('C0000001', 'P0000007')('C0000002', 'P0000009')('C0000003','P0000010', ('C0000004','P0000011'),('C0000005','P0000013'),('C0000006','P0000015'),
+('C0000001', 'P0000007'),('C0000002', 'P0000009'),('C0000003','P0000010'), ('C0000004','P0000011'),('C0000005','P0000013'),('C0000006','P0000015'),
 ('C0000007','P0000016'),('C0000008','P0000017'),('C0000009','P0000018'),('C0000010','P0000019'),('C0000011','P0000020'),('C0000012','P0000021'),
-('C000013','P0000022'),('C000014','P0000023'),('C0000015','P0000024');
+('C0000013','P0000022'),('C0000014','P0000023'),('C0000015','P0000024');
 
 INSERT INTO Breeds (BreedID, name) VALUES
 ('BR000001', 'Bull Terrier'),('BR000002', 'American Bully'),('BR000003', 'Pitbull'), ('BR000004', 'English Bulldog');
@@ -116,19 +116,19 @@ VALUES ('D0000001', 'Bronx', 'B0000001', 'BR000002', 'T0000001', null ,null, nul
 ('D0000005', 'Prada', 'B0000001', 'BR000001', 'T0000001', null ,null, null, 80, 'MALE', 'FALSE'), ('D0000006', 'Sandy', 'B0000002', 'BR000003', 'T0000002',null,null, null, 100, 'MALE', 'FALSE'),
 ('D0000007', 'Danger', 'B0000003', 'BR000003', 'T0000001',null ,null, null, 62, 'MALE', 'FALSE'), ('D0000008', 'Chanel', 'B0000003', 'BR000003', 'T0000004',null,null, null, 20, 'FEMALE', 'TRUE'),
 ('D0000009', 'Banga', 'B0000004', 'BR000001', 'T0000002',null,null, null, 59, 'FEMALE', 'SOLD'),('D0000010', 'Nino', 'B0000005', 'BR000004', 'T0000002',null,null, null, 33, 'MALE', 'FALSE'),
-('D0000011', 'Bow Leg Lou', 'B0000002', 'BR000005', 'T0000005',null,null, null, 58, 'FEMALE', 'FALSE'),('D0000012', 'Kurt', 'B0000004', 'BR000004', 'T0000006',null,null, null, 45, 'MALE', 'FALSE'),
+('D0000011', 'Bow Leg Lou', 'B0000002', 'BR000004', 'T0000005',null,null, null, 58, 'FEMALE', 'FALSE'),('D0000012', 'Kurt', 'B0000004', 'BR000004', 'T0000006',null,null, null, 45, 'MALE', 'FALSE'),
 ('D0000013', 'AK', 'B0000004', 'BR000002', 'T0000003',null,null, null, 63, 'MALE', 'FALSE'),('D0000014', 'Yaya', 'B0000001', 'BR000002', 'T0000005',null,null, null, 70, 'FEMALE', 'FALSE'),
 ('D0000015', 'Richie', 'B0000001', 'BR000001', 'T0000001',null,null, null, 30, 'MALE', 'FALSE'),('D0000016', 'Nina', 'B0000003', 'BR000003', 'T0000001',null,null, null, 26, 'FEMALE', 'TRUE'),
 ('D0000017', 'Robbie', 'B0000004', 'BR000004', 'T0000002',null,null, null, 20, 'MALE', 'TRUE'),('D0000018', 'Ruv', 'B0000004', 'BR000003', 'T0000004',null,null, null,  54, 'MALE', 'TRUE');
 
 INSERT INTO Puppies(PuppyID, DogID)
 VALUES
-('PP000001', 'D0000008'), ('PP000002', 'D0000010'), ('PP000003', 'D0000015'), ('PP000004', 'D0000016'),('PP000004', 'D0000017');
+('PP000001', 'D0000008'), ('PP000002', 'D0000010'), ('PP000003', 'D0000015'), ('PP000004', 'D0000016'),('PP000005', 'D0000017');
 
 INSERT INTO AdultDogs(AdultDogID, DogID)
 VALUES
 ('AD000001','D0000001'), ('AD000002','D0000002'),('AD000003','D0000003'),('AD000004','D0000004'),('AD000005','D0000005'),('AD000006','D0000006'),('AD000007','D0000007'),
-('AD000008','D0000009'),('AD000009','D0000011'),('AD000010','D0000012'),('AD000011','D0000013'),('AD000012','D0000014'),('AD000012','D0000018');
+('AD000008','D0000009'),('AD000009','D0000011'),('AD000010','D0000012'),('AD000011','D0000013'),('AD000012','D0000014'),('AD000013','D0000018');
 
 
 INSERT INTO PURCHASES(PurchaseID,CustomerID, DogID, PriceUSD, PurchaseDate) VALUES
@@ -146,13 +146,13 @@ INSERT INTO PURCHASES(PurchaseID,CustomerID, DogID, PriceUSD, PurchaseDate) VALU
 -- ('PU000023','C0000023','D0000009', 1000.00, null);
 
 INSERT INTO StudPurchases(StudPurchaseID, PurchaseID) VALUES
-('SP000001', 'PU000002'),('SP000002', 'PU000013'),('SP000003', 'PU000016'),('SP000003', 'PU0000012'), ('SP000004','PU000007'), ('SP00005', 'PU0000015'),
-('SP00006','PU000003'),('SP000007', 'PU000004'), ('SP00008', 'PU0000012');
+('SP000001', 'PU000002'),('SP000002', 'PU000013'),('SP000003', 'PU000016'),('SP000004', 'PU000012'), ('SP000005','PU000007'), ('SP000006', 'PU000015'),
+('SP000007','PU000003'),('SP000008', 'PU000004'), ('SP000009', 'PU000012');
 
 
 
 INSERT INTO DogPurchases(DogPurchaseID, PurchaseID ) VALUES
-('DP000001','PU000005'), ('DP000002','PU000014'), ('DP000003','PU000005'), ('DP000004','PU000008'), ('DP000005','PU0009'),
+('DP000001','PU000005'), ('DP000002','PU000014'), ('DP000003','PU000005'), ('DP000004','PU000008'), ('DP000005','PU000009'),
 ('DP000006','PU000010');
 -- CREATE OR REPLACE FUNCTION get_breeders_dogs (char(4), REFCURSOR) returns refcursor as
 -- $$
@@ -175,6 +175,7 @@ INSERT INTO DogPurchases(DogPurchaseID, PurchaseID ) VALUES
 
 
 -- GET DOGS THAT ARE SIBLINGS
+
 
 
 CREATE OR REPLACE FUNCTION most_expensive_dogs (char(4), char(4), REFCURSOR) returns refcursor as
@@ -219,12 +220,22 @@ $$
 language plpgsql;
 
 
+
+--VIEW: List all of the puppies for sale
+
+/*
+CREATE VIEW AvailablePuppiesForSale  AS
+SELECT name, weightlbs, gender from dogs inner join puppies on dogs.dogid = puppies.dogid
+where dogs.forsale = 'TRUE';
+*/
+
+
 -- POSSIBLE TRIGGERS: WHENEVER A NEW  PUPPY IS PURCHASED, automatically set, forSALE to be SOLD
 
 
 -- VIEWS SELECT THE  dog with the most children, this is for people that are interested in the lineage of a specfic dog
 
-
+--
 
 -- CREATE VIEW CurrentDogs  AS
 -- SELECT name, dob, gender, weightlbs,  from dogs inner join adultdogs on dogs.dogid = puppies.dogid
@@ -232,8 +243,33 @@ language plpgsql;
 -- order by desc;
 -- -- view the trainer that has trained the most expensive dog
 
+/* View the current trainer that sold the most expensive stud */
 
--- CREATE VIEW ExpensiveTrainer as
--- SELECT name from people inner join trainers on trainers.peopleid = people.peopleid inner join dogs on dogs.trainerid = trainers.trainerid inner join purchases on purchases.dogid = dogs.dogid where  purchases.priceusd = (select max(priceusd) from purchases);
-i
+
+CREATE VIEW ExpensiveStudTrainer
+SELECT people,name, purchases.priceusd from people inner join Trainers
+on trainers.personid = people.Person inner join dogs
+on dogs.trainerid = trainers.trainerid
+inner join purchases on purchases.dogid = dogs.dogid inner join studpurchases on
+purchases.purchaseid = studpurchases.PurchaseID
+where purchases.priceusd = (select max(priceusd) from purchases);
 -- VIEWS
+
+
+--SECURITY
+
+
+CREATE ROLE Breeders;
+GRANT ALL ON ALL TABLES
+TO ADMIN;
+
+--Breeders are the people managing each dog in BBT, therefore they have ultimate access to everything
+
+CREATE ROLE trainers;
+grant select on dogs, adultdogs, puppies, breeds;
+  TO trainers;
+
+
+-- REPORTS
+
+--QUERY TO RETURN the percentage of dogs that are pitbulls
